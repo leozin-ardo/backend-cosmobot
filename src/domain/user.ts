8e401eefb094cd5f22c7f70adcea93790f0533de
Leonardo A R De Oliveira <leonardo.oliveira@negocioanimal.com.br>
2022-08-05 07:38:31 -0300

    feat(models): create user, group models and uuid provider

---
+++ src/domain/user.ts
@@ -0,0 +1,21 @@
+import { UuidProvider } from "@/main/factories/uuid-provider";
+
+export class UserModel {
+    readonly id: string;
+    first_name!: string;
+    last_name!: string;
+    email!: string;
+    group_id!: string;
+
+    constructor(user: userFactory) {
+        this.id = UuidProvider.generate();
+        Object.assign(this, user);
+    }
+}
+
+type userFactory = {
+    firstName: string;
+    lastName: string;
+    email: string;
+    group_id: string;
+};
